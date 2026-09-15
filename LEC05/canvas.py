4d3f07871f3a6c52c706ccce013976b2f64ebf85
jogomme <beomgih945@gmail.com>
2026-09-15 16:34:58 +0900

    캐릭터 이동

--- LEC05/canvas.py
+++ LEC05/canvas.py
@@ -17,16 +17,25 @@ from pico2d import *
 
 open_canvas(800, 600)
 
-chatacter = load_image('character.png')
-chatacter.draw(400,300)
-chatacter.draw(300,200)
-chatacter.draw(500,400)
+# load_image('파일명') 으로 이미지를 가져옴
+grass = load_image('grass.png')
+character = load_image('character.png')
 
-# 매 프레임마다 update__canvas()를 해줘야 한다
-update_canvas()
+x = 0
 
-# 5초 지연
-delay(5)
-
+while x < 800 :
+    #캔버스를 지워준다
+    clear_canvas()
+    
+    # 배경과 캐릭터 중 배경을 먼저 그려야 한다.
+    # 그래야 캐릭터가 배경에 가려지지 않는다.
+    grass.draw(400, 30)
+    character.draw(x, 90)
+    
+    # 매 프레임마다 update__canvas()를 해줘야 한다
+    update_canvas()
+    x += 2
+    delay(0.01)
+    
 # 캔버스를 닫는다.
 close_canvas()
