8e356eef17a2604a5aa79f476ed8aa76dcd80c34
jogomme <beomgih945@gmail.com>
2026-09-15 16:57:42 +0900

    실습과제 1 : 캐릭터가 사각형 그리기

--- LEC05/canvas.py
+++ LEC05/canvas.py
@@ -4,38 +4,50 @@
 from pico2d import *
 
 # open_canvas 로 인해 800x600짜리 캔버스가 열린다.
-'''
-좌표계 
-(0,599) 
-
-       
-             (400,300)
-
-
-(0,0)                          (799,0)
-'''
-
 open_canvas(800, 600)
 
 # load_image('파일명') 으로 이미지를 가져옴
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
+# 게임이 실행 중인지 알려주는 변수
+is_game_on = True
 
-while x < 800 :
-    #캔버스를 지워준다
+Points = [200,100]
+
+character.draw(Points[0], Points[1])
+
+
+while is_game_on :
+    # 백지 상태로 만들기
     clear_canvas()
     
-    # 배경과 캐릭터 중 배경을 먼저 그려야 한다.
-    # 그래야 캐릭터가 배경에 가려지지 않는다.
-    grass.draw(400, 30)
-    character.draw(x, 90)
+    # 사각형의 경로 만들기
+    draw_rectangle(200,100,600,500)
     
-    # 매 프레임마다 update__canvas()를 해줘야 한다
+    # 조건문
+    if Points[0] < 600 and Points[1] <= 100:
+        Points[0] += 2
+    elif Points[0] == 600 and Points[1] < 500 :
+        Points[1] += 2
+    elif Points[0] > 200 and Points[1] == 500 :
+        Points[0] -= 2
+    elif Points[0] == 200 and Points[1] > 100 :
+        Points[1] -= 2
+    
+    character.draw(Points[0], Points[1])
+    
+    
     update_canvas()
-    x += 2
     delay(0.01)
     
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                is_game_on = False
+    
+        
 # 캔버스를 닫는다.
 close_canvas()
