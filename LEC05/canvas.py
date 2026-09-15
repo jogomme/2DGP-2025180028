bbb0fec279ff9a50b7c74ad73bd73cd873bdb337
jogomme <beomgih945@gmail.com>
2026-09-15 16:10:17 +0900

    pico2d 기본적인 캔버스 그리는 방법

--- LEC05/canvas.py
+++ LEC05/canvas.py
@@ -1,3 +1,16 @@
 ## 여기를 채우시오.
 
 
+from pico2d import *
+
+# open_canvas 로 인해 800x600짜리 캔버스가 열린다.
+open_canvas(800, 600)
+
+# 매 프레임마다 update__canvas()를 해줘야 한다
+update_canvas()
+
+# 5초 지연
+delay(5)
+
+# 캔버스를 닫는다.
+close_canvas()
