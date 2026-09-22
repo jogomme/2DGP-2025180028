d536af22e9c2893c94b1b8ed89f809a532e9ac83
jogomme <beomgih945@gmail.com>
2026-09-22 16:31:58 +0900

    원-사각-삼각 운동으로 이루어지는 전체 흐름 완성

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,20 @@
 # 실습 과제 진행
 
+def move_circle() :
+    print(f"circle")
+    pass
+
+def move_rectangle() :
+    print("rectangle")
+    pass
+
+def move_triangle() :
+    print("triangle")
+    pass
+
 while True :
+    a = '\0'
+    move_circle()
+    move_rectangle()
+    move_triangle()
     pass
